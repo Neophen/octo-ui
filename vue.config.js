823d6b0a0865bc8043326d99691c96768d996bd1
Mykolas Mankevcius <mykolas@unity3d.com>
2020-03-17 00:10:45 +0200

    Updating browser list

--- vue.config.js
+++ vue.config.js
@@ -10,15 +10,12 @@ module.exports = {
 
   configureWebpack: {
     output: { globalObject: "this" },
-    externals: ["vue", "@vue/composition-api", "@popperjs/core", "portal-vue"]
+    externals: [
+      "vue",
+      "@vue/composition-api",
+      "@popperjs/core",
+      "portal-vue",
+      "core-js"
+    ]
   }
-  // css: {
-  //   loaderOptions: {
-  //     sass: {
-  //       data: `
-  //         @import "@/scss/_variables.scss";
-  //       `
-  //     }
-  //   }
-  // }
 };
